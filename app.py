5414f68f24f57abacfb9dc9e0961c980ed3f493f
DivingKitten <Unility@163.com>
2019-12-24 21:32:05 +0800

    添加了表单

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
 from flask import Flask
 from flask import render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hardtoguess'
 bootstrap = Bootstrap(app)
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
 
-@app.route('/hello/<name>')
-def hello2(name):
+@app.route('/hello', methods=['GET', 'POST'])
+def hello2():
     # return 'hello my def %s' % name
-    return render_template('user.html', name=name)
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('user.html', form=form, name=name)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def page_not_found(e):
