9477078faaca8b42e845e0f3334c487ec0e6a21b
DivingKitten <Unility@163.com>
2019-12-24 21:00:36 +0800

    添加了自定义的错误页面

--- app.py
+++ app.py
@@ -15,6 +15,14 @@ def hello2(name):
     # return 'hello my def %s' % name
     return render_template('user.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
