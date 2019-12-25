9b3a999361c2746dbd9becfa171e257108c36414
DivingKitten <Unility@163.com>
2019-12-25 17:28:07 +0800

    添加了注释

--- app.py
+++ app.py
@@ -6,20 +6,22 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+#在wtf的模块中防止表单的跨站点攻击，设置一个验证加密串
 app.config['SECRET_KEY'] = 'hardtoguess'
+#调用bootstrap模块
 bootstrap = Bootstrap(app)
 
-
+#自定义表单的数据格式和预设内容
 class NameForm(Form):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-
+#路由函数
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
-
+#路由函数
 @app.route('/hello', methods=['GET', 'POST'])
 def hello2():
     # return 'hello my def %s' % name
@@ -30,12 +32,12 @@ def hello2():
         form.name.data = ''
     return render_template('user.html', form=form, name=name)
 
-
+#资源错误处理函数
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-
+#服务器异常处理函数
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
